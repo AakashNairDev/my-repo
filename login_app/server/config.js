413ed4e721d9a4b3a865e53a29545984426f028f
Aakash.N <aakashnair.im@gmail.com>
2024-02-01 13:11:11 +0530

    Removed the secrets

--- login_app/server/config.js
+++ login_app/server/config.js
@@ -1,6 +1,6 @@
 export default {
-    JWT_SECRET : "J/R57Y85l6bWf9ceusgQ0xo4h5P2arA0TdIj3n4H4yQ=",
-    EMAIL: "mortimer.adams@ethereal.email",
-    PASSWORD: "4QhbyjtDZkfWqGtFPe",
-    ATLAS_URI: "mongodb+srv://itachistarck:AXM8zlwfLwOWGuPS@demo-db.hqsuig8.mongodb.net/?retryWrites=true&w=majority"
+    JWT_SECRET : "xxxxx",
+    EMAIL: "xxxxxxx",
+    PASSWORD: "xxxxxx",
+    ATLAS_URI: "xxxxxxx"
 }
